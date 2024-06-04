eb0e918fed2725935658bdbfe6c516a6a75b987e
sandrandombi <sandrandnz@gmail.com>
2024-06-04 08:47:35 +0200

    modules

---
+++ main.js
@@ -0,0 +1,8 @@
+import addition from "./add.js";
+console.log(addition);
+
+
+import { saluer, name } from "./variables.js";
+addition("abc", "efg")
+console.log(addition (3,5));
+saluer( name)
---
+++ variables.js
@@ -0,0 +1,4 @@
+export const name = "jean"
+export function saluer(a){
+    console.log("bonjour" + a)
+}
